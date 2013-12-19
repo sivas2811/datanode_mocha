38419b17cb75e79de6d8f0e9b29b860815273806
Sivasubramanian.R <sivas@cs.wisc.edu>
2013-12-18 18:21:26 -0600

    caps

--- comp.sh
+++ comp.sh
@@ -1,3 +1,3 @@
 git pull
 javac DataNode.java
-java -cp mysql-connector-java-5.1.27/mysql-connector-java-5.1.27-bin.jar DataNode
+java -cp ~/mysql-connector-java-5.1.27/mysql-connector-java-5.1.27-bin.jar DataNode
